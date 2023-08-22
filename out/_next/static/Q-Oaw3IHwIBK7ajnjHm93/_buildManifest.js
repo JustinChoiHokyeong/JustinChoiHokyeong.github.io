e8ade7cbb12f863f876cb8d4be3233476ef9605e
최호경 <chhk7449@gmail.com>
2023-08-22 14:47:49 +0900

    deploy to gh-pages

---
+++ out/_next/static/Q-Oaw3IHwIBK7ajnjHm93/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/78-bc6272765d0ab07c.js","static/css/b04218d57dd918ef.css","static/chunks/pages/index-42bdde4115e67e0f.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
